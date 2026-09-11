1333349abd84e6c1230ba568ba98342a4ff6255e
Keisuke Urakari <urakari@benest.co.jp>
2026-09-11 11:52:29 +0900

    仕様書PDFダウンロードスクリプト (getspecific.sh) を追加

---
+++ getspecific.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+mkdir -p specific
+
+# yamaha
+
+curl -L --output-dir ./specific -O https://jp.yamaha.com/files/download/other_assets/0/321740/xg_v135_j.pdf
+curl -L --output-dir ./specific -O https://jp.yamaha.com/files/download/other_assets/8/321748/xgparameterchangetable.pdf
+curl -L --output-dir ./specific -O https://jp.yamaha.com/files/download/other_assets/4/321754/xgmap.pdf
+curl -L --output-dir ./specific -O https://jp.yamaha.com/files/download/other_assets/9/321749/voice_list.pdf
+curl -L --output-dir ./specific -O https://jp.yamaha.com/files/download/other_assets/3/321753/voiceextension.pdf
+curl -L --output-dir ./specific -O https://jp.yamaha.com/files/download/other_assets/1/321741/spec.pdf
+curl -L --output-dir ./specific -O https://jp.yamaha.com/files/download/other_assets/2/321742/page_top.pdf
+curl -L --output-dir ./specific -O https://jp.yamaha.com/files/download/other_assets/6/321746/efctparamtbl.pdf
+curl -L --output-dir ./specific -O https://jp.yamaha.com/files/download/other_assets/5/321745/efctparamlist.pdf
+curl -L --output-dir ./specific -O https://jp.yamaha.com/files/download/other_assets/7/321747/efctparamdeflt.pdf
+curl -L --output-dir ./specific -O https://jp.yamaha.com/files/download/other_assets/3/321743/efctmap.pdf
+curl -L --output-dir ./specific -O https://jp.yamaha.com/files/download/other_assets/0/321750/drumvoicelist.pdf
+curl -L --output-dir ./specific -O https://jp.yamaha.com/files/download/other_assets/1/321751/drum_default.pdf
+curl -L --output-dir ./specific -O https://jp.yamaha.com/files/download/other_assets/2/321752/drum_default_ext.pdf
+curl -L --output-dir ./specific -O https://jp.yamaha.com/files/download/other_assets/6/321756/xgsongdata.pdf
+curl -L --output-dir ./specific -O https://jp.yamaha.com/files/download/other_assets/9/321739/read_aoyama.pdf
+
+# roland
+curl -L --output-dir ./specific -O https://cdn.roland.com/assets/media/pdf/SC-55_OM.pdf
+curl -L --output-dir ./specific -O https://cdn.roland.com/assets/media/pdf/SC-88_OM.pdf
+curl -L --output-dir ./specific -O https://cdn.roland.com/assets/media/pdf/SC-88ST_OM.pdf
